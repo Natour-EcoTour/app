920e2e329a1fd56fbc9aaf5d3603d0ab1828990c
Vitor <vitorantunes2003@gmail.com>
2025-03-21 00:09:59 -0300

    design: added markers on map

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,11 +1,43 @@
 import React from 'react';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
 
-export default function AboutScreen() {
+const markers = [
+  {
+    id: 1,
+    title: 'Pico do urubu',
+    description: 'Trilha de caminhada',
+    coordinate: { latitude: -23.484787, longitude: -46.206867 },
+    icon: require('../../assets/images/favicon.png'),
+  },
+  {
+    id: 2,
+    title: 'Sítio do Seu Joaquim',
+    description: 'Sítio',
+    coordinate: { latitude: -23.474011, longitude: -46.216179 },
+    icon: require('../../assets/images/favicon.png'),
+  },
+];
+
+export default function App() {
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <MapView style={styles.map} initialRegion={{
+        latitude: -23.550377,
+        longitude: -46.633940,
+        latitudeDelta: 6,
+        longitudeDelta: 6,
+      }}>
+        {markers.map((marker) => (
+          <Marker
+            key={marker.id}
+            coordinate={marker.coordinate}
+            title={marker.title}
+            description={marker.description}
+            image={marker.icon}
+          />
+        ))}
+      </MapView>
     </View>
   );
 }
