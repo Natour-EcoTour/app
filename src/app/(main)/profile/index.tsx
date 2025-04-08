2673a646635659257aa25acbfe7a13c598a27397
Vitor <vitorantunes2003@gmail.com>
2025-04-08 00:41:04 -0300

    wip: pfp image picker

--- src/app/(main)/profile/index.tsx
+++ src/app/(main)/profile/index.tsx
@@ -1,10 +1,33 @@
-import { Text, View,  StyleSheet } from 'react-native';
-import { Link } from 'expo-router'
+import { Text, View, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { useState } from 'react';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function Banana() {
+  // https://docs.expo.dev/tutorial/image-picker/
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+  
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>PERFIL</Text>
+      <TouchableOpacity onPress={pickImageAsync}>
+        <Text style={styles.text}>PERFIL</Text>
+      </TouchableOpacity>
+      {selectedImage && (
+        <Image source={{ uri: selectedImage }} style={styles.image} />
+      )}
     </View>
   );
 }
@@ -18,6 +41,11 @@ const styles = StyleSheet.create({
   },
   text: {
     color: '#fff',
+  },
+  image: {
+    width: 200,
+    height: 200,
+    marginTop: 20,
   },
   button: {
     fontSize: 20,
