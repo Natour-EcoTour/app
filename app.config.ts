1ae3f6c8f32b16f6bd2d2c1c9696fd5528b09da9
FlamingoLindo <vitorantunes2003@gmail.com>
2025-09-27 13:06:57 -0300

    1

--- app.config.ts
+++ app.config.ts
@@ -1,4 +1,8 @@
-import type { ExpoConfig } from "expo/config";
+import { ExpoConfig, ConfigContext } from 'expo/config';
+
+const EAS_PROJECT_ID = "4874ed03-7bf0-400f-84ae-e948d17e3a08"
+const PROJECT_SLUG = "natour_app"
+const OWNER = "flamingo_lindo"
 
 export default ({ config }: { config: ExpoConfig }) => ({
   ...config,
